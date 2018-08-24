b24d5b3430a6bcd8189bf73156956ab93ce7bddd
victorjefferies <victor.jefferies@live.com>
2018-08-24 13:42:39 +0000

    Added November as default input

--- directory.rb
+++ directory.rb
@@ -1,18 +1,4 @@
 student_count = 11
-# let's put all students into an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 
 def input_students
   puts "Please enter the names of the students"
@@ -21,22 +7,21 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
-  # get hobbies
-  puts "Students hobbies"
-  hobbies = gets.chomp
-  puts "Students country of origin"
-  country = gets.chomp
   # while the name is not empty, repeat this code
+  puts "What is your cohort"
+  cohort = gets.chomp
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november, hobbies: hobbies, country: country}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     # get another name from the user
+    puts "Name"
     name = gets.chomp
-    puts "What hobbies do you have?"
-    hobbies = gets.chomp
-    puts "What country do you come from"
-    country = gets.chomp
+    puts "Cohort"
+    cohort = gets.chomp
+    if !name.empty? && cohort.empty?
+      cohort = "november"
+    end
   end
   # return the array of students
   students

93fb4d4ec0fd8876b152a64216f8605d636209ff
victorjefferies <victor.jefferies@live.com>
2018-08-24 10:04:54 +0000

    Added input_students to allow student hash to be updated

--- directory.rb
+++ directory.rb
@@ -13,6 +13,26 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
 def print_header
   puts "The students of Villains Academy"
   puts "-------------"
@@ -26,6 +46,7 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
